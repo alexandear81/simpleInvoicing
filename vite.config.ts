739759fbb639c33908274d15ff27b55f6700a61e
Alex Guryanov <alexandear81@gmail.com>
2025-07-17 15:33:02 +0200

    Fix: Vercel build issues and path alias2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
